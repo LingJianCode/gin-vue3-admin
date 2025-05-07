9917a1499aefc273d2741033ae2ee4f3aa25dd63
lingjian <lingjian2016@163.com>
2025-05-07 10:14:25 +0800

    fix: api options group

--- response/sys_api.go
+++ response/sys_api.go
@@ -2,10 +2,9 @@ package response
 
 type ApiOption struct {
 	// 选项的值
-	Value uint `json:"value,omitempty"`
+	Value uint `json:"value"`
 	// 选项的标签
-	Label    string `json:"label,omitempty"`
-	Disabled bool   `json:"disabled,omitempty"`
+	Label string `json:"label,omitempty"`
 	// 子选项列表
 	Children []*ApiOption `json:"children,omitempty"`
 }
--- service/sys_api.go
+++ service/sys_api.go
@@ -8,16 +8,16 @@ import (
 
 func GetApiOptions() (apiOptions []response.ApiOption, err error) {
 	var apiList []models.SysApi
-	err = global.OPS_DB.Find(&apiList).Error
+	err = global.OPS_DB.Order("id").Find(&apiList).Error
 	apiMap := make(map[string][]models.SysApi)
 	for _, v := range apiList {
 		apiMap[v.Group] = append(apiMap[v.Group], v)
 	}
 	for key, val := range apiMap {
 		api := response.ApiOption{
-			Label:    key,
-			Value:    0,
-			Disabled: true,
+			Label: key,
+			// value为0的不是api接口
+			Value: 0,
 		}
 		for _, v := range val {
 			api.Children = append(api.Children, &response.ApiOption{
